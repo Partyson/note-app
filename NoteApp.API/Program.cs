44089e36c1a6cd2668e7a6ef6526127a3ef6e7c9
KabAchOk <bogdan.shustik1@mail.ru>
2025-06-16 18:17:19 +0500

    fix: fic connect

--- NoteApp.API/Program.cs
+++ NoteApp.API/Program.cs
@@ -26,13 +26,7 @@ builder.Services.AddControllers()
 
 builder.Services.AddOpenApi();
 
-var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
-var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
-var dbName = Environment.GetEnvironmentVariable("DB_NAME");
-var dbUser = Environment.GetEnvironmentVariable("DB_USER");
-var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
-
-var connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPassword}";
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString,
